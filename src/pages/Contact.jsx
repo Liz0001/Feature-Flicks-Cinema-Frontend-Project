d55eb33e777e7ea240c065c167b8574a7d61c50a
Liis U <liis.usin@gmail.com>
2023-03-26 00:40:15 +0100

    contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,36 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 
 export default function Contact() {
 
   return <>
-    <h2>Contact up</h2>
-    <p>Call or email or come and visit us!</p>
+    <h2 className="mb-4">Contact us</h2>
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Topic</Form.Label>
+
+        <Form.Control type="text" placeholder="Booking issue, movie question, ..." />
+      </Form.Group>
+
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+
+      <Button variant="outline-primary" type="submit">
+        Send
+      </Button>
+    </Form>
+
+
   </>
 
 }
